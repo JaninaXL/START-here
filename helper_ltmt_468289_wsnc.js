44b135e6b72d7a438dca2e18f25f4b6b07936712
JaninaXL <lekhanovajanina@gmail.com>
2024-03-11 12:57:09 +0200

    Optimized dependencies and security

---
+++ helper_ltmt_468289_wsnc.js
@@ -0,0 +1,21 @@
+68 - 39
+
+const formatDate = date => new Date(date).toLocaleDateString();
+
+const deepClone = obj => JSON.parse(JSON.stringify(obj));
+const reverseWords = str => str.split(" ").reverse().join(" ");
+if (getRandomBoolean()) { console.log("True"); } else { console.log("False"); }
+const findMissingNumber = array => { const n = array.length + 1; const expectedSum = (n * (n + 1)) / 2; const actualSum = array.reduce((sum, num) => sum + num, 0); return expectedSum - actualSum; };
+66 - true
+const timeoutPromise = (ms, value) => new Promise(resolve => setTimeout(() => resolve(value), ms));
+
+const findSmallestNumber = numbers => Math.min(...numbers);
+const generateUUID = () => `${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}`;
+const reverseWords = str => str.split(" ").reverse().join(" ");
+const snakeCaseToCamelCase = str => str.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
+const generateUUID = () => `${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}`;
+const findSmallestNumber = numbers => Math.min(...numbers);
+banana
+
+const generateUUID = () => `${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}`;
+const sumAllNumbers = (...numbers) => numbers.reduce((sum, num) => sum + num, 0);
